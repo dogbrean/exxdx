8aa9db40732a784fbefbbea2aa3edeef1ec56a70
dongju.lim <dongju.lim@nts-corp.com>
2025-10-05 21:08:39 +0900

    코드 정리

--- src/components/WelcomeCurtain/WelcomeCurtain.tsx
+++ src/components/WelcomeCurtain/WelcomeCurtain.tsx
@@ -7,25 +7,26 @@ import { WELCOME_ANIMATION_TIME } from "@/constants/ui";
 
 const WelcomeCurtain = () => {
   const [isAnimated, setIsAnimated] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     setIsAnimated(true);
     const timeoutId = setTimeout(() => {
-      setIsVisible(true);
+      setIsVisible(false);
     }, WELCOME_ANIMATION_TIME);
 
     return () => clearTimeout(timeoutId);
   }, []);
 
-  return isVisible ? null : (
+  return isVisible ? (
     <div className={cn(styles.welcome_curtain, isAnimated && styles.animated)}>
       <div className={cn(styles.left)}></div>
       <div className={cn(styles.right)}></div>
+      <div className={cn(styles.line)}></div>
       <span className={cn(styles.text_left)}>□∃xx</span>
       <span className={cn(styles.text_right)}>d(x)</span>
     </div>
-  );
+  ) : null;
 };
 
 export default WelcomeCurtain;
